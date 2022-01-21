e7af0ff3f98991d13dad2a40d04529fc703d3da4
CharLemAznable <545541819@qq.com>
2022-01-21 18:08:29 +0800

    Fixed test log output, some refactor.

--- luna-cloud-producer/src/test/java/com/github/charlemaznable/luna/producer/ProducerServer.java
+++ luna-cloud-producer/src/test/java/com/github/charlemaznable/luna/producer/ProducerServer.java
@@ -2,12 +2,10 @@ package com.github.charlemaznable.luna.producer;
 
 import com.github.charlemaznable.luna.commons.LunaApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @LunaApplication
-@EnableDiscoveryClient
 @EnableEurekaServer
 @EnableFeignClients
 public class ProducerServer {
